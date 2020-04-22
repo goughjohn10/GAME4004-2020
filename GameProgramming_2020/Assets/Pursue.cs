94aab143c7555f50d0c24bafc1a77831b4497f26
goughjohn10 <goughjohn10@gmail.com>
2020-04-22 14:29:52 +0100

    Lecture Started
    NavMesh down, starting tile stuff

---
+++ GameProgramming_2020/Assets/Pursue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pursue : MonoBehaviour
+{
+    private NavMeshAgent navAgent;
+
+    public Transform goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navAgent.destination = goal.position;
+    }
+}
